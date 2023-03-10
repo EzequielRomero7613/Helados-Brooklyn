163a519bd14acc38951c062696efee2e0d3f0c43
EzequielRomero7613 <romeroeze7613@gmail.com>
2023-03-10 16:25:12 -0300

    primer comit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+function horarios() {
+    document.getElementById("horarios")
+    alert("Lunes: De 13:20 a 23:40 , Martes: Cerrado, Miércoles: De 13:20 a 23:40, Jueves: De 13:20 a 23:40, Viernes: De 13:20 a 23:40, Sábado: De 13:20 a 23:40, Domingo: De 13:00 a 23:40");
+}
